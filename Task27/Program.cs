ab8e6daad8b213c730de3ff6c686b8040d5a1f3f
Peter-XXL-2022 <109310363+Peter-XXL-2022@users.noreply.github.com>
2022-08-17 12:14:30 +0300

    Add files via upload

---
+++ Task27/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+int inputNumber = GetInteger("Введите число: ");
+int sumDigits = SumDigits(inputNumber);
+Console.WriteLine($"Сумма цифр числа {inputNumber}: {sumDigits}");
+
+int GetInteger(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumDigits(int number)
+{
+    int result = 0;
+
+    while (number != 0)
+    {
+        result += number % 10;
+        number /= 10;
+    }
+
+    return result;
+}
